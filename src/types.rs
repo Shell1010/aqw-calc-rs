db24e7486aa023af82bb083528cf15a02b0fe77c
Shell1010 <amin.dev03@gmail.com>
2024-09-16 02:07:59 +0000

    half assed

---
+++ src/types.rs
@@ -0,0 +1,119 @@
+#[allow(dead_code)]
+pub struct Character {
+    all_out: f32,
+    phy_out: f32,
+    mag_out: f32,
+    heal_out: f32,
+}
+
+impl Character {
+    pub fn new(all_out: f32, phy_out: f32, mag_out: f32, heal_out: f32) -> Character {
+        Self { all_out, phy_out, mag_out, heal_out}
+    }
+}
+
+pub struct Enemy {
+    all_in: f32,
+    phy_in: f32,
+    mag_in: f32,
+}
+
+impl Enemy {
+    pub fn new(all_in: f32, phy_in: f32, mag_in: f32) -> Enemy {
+        Self { all_in, phy_in, mag_in }
+    }
+}
+
+
+pub struct Skill {
+    pub name: String,
+    pub coef: f32,
+    pub function: Function,
+    pub is_heal: bool
+}
+
+impl Skill {
+    pub fn new(name: &str, coef: f32, function: Function) -> Skill {
+        Self { name: name.to_string() , coef, function, is_heal: coef.is_sign_negative() }
+    }
+
+    pub fn calculate(&self, wdps: f32) -> f32 {
+        self.coef * &self.function.calculate(wdps)
+    }
+}
+
+
+pub enum FuncType {
+    Physical,
+    Magical,
+    Hybrid
+}
+
+pub enum Function {
+    AP1 { ap: f32,  },
+    SP1 { sp: f32,  },
+    AP2 { ap: f32, w_range: f32,  },
+    SP2 { sp: f32, w_range: f32,  },
+}
+
+impl Function {
+    pub fn calculate(&self, wdps: f32 ) -> f32 {
+        match self {
+            Function::AP1 { ap, } => wdps + (0.1 * ap),
+            Function::SP1 { sp, } => wdps + (0.1 * sp),
+
+            Function::AP2 { ap, w_range, } => {
+                let ap1 = wdps + (0.1 * ap);
+                2.0 * ap1 * w_range
+            }
+
+            Function::SP2 { sp, w_range, } => {
+                let sp1 = wdps + (0.1 * sp);
+                2.0 * sp1 * w_range
+            }
+        }
+    }
+
+    pub fn get_type(&self) -> FuncType {
+
+        match self {
+            Function::AP1 { ap: _, } => FuncType::Physical,
+            Function::SP1 { sp: _, } => FuncType::Magical,
+            Function::SP2 { sp: _, w_range: _, } => FuncType::Magical,
+            Function::AP2 { ap: _, w_range: _, } => FuncType::Physical,
+        }
+    }
+}
+
+pub struct Calculator {
+    me: Character,
+    enemy: Enemy,
+    skill: Skill,
+}
+
+impl Calculator {
+    pub fn new ( me: Character, enemy: Enemy, skill: Skill ) -> Calculator {
+        Self { me, enemy, skill }
+    }
+
+    pub fn calculate(&self, wdps: f32) -> i32 {
+
+        let func_type = self.skill.function.get_type();
+
+        let val = match func_type {
+            FuncType::Hybrid => 
+                self.skill.calculate(wdps) * (self.me.all_out * self.me.phy_out ) * (self.enemy.all_in * self.enemy.phy_in ),
+            FuncType::Magical => 
+                self.skill.calculate(wdps) * (self.me.all_out * self.me.mag_out ) * (self.enemy.all_in * self.enemy.mag_in ),
+            FuncType::Physical => 
+                self.skill.calculate(wdps) * (self.me.all_out * self.me.phy_out ) * (self.enemy.all_in * self.enemy.phy_in )
+
+        };
+        if self.skill.is_heal {
+            (val * self.me.heal_out) .round() as i32
+        } else {
+            val.round() as i32
+        }
+        
+    }
+}
